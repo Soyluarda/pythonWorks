8c4d74bf601e2133a2e3f1db14254449ffd6d253
Iyz <ardasoylu39@hotmail.com>
2018-10-01 11:16:48 +0300

    added new functional parameters to comment-url, function created and iterate wth Thread

--- requests-working.py
+++ requests-working.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
 import requests
 import json
+from threading import Thread
 
-url = "yoururl.com"
+def add_comment(id,post_id):
+    ## veri almak istedigimiz url'i customer_list_url olarak tanımlıyoruz. isteklerimizi cekmek icin request kütüphanesi ile bu url'deki verileri customer_list'e atıyoruz ve json objesine ceviriyoruz.
+    customer_list_url = "http://yourapi.com/api/get-customer-list"
+    customer_list = requests.get(customer_list_url)
+    json_customers = json.loads(customer_list.text)
 
-r = requests.get(url)
-print r.status_code
+    #yeni olusturdugumuz customer dictionary'e customer_listteki token ve id'leri ekliyoruz.
+    customer_lists = []
+    for i in json_customers:
+        cust = {"token":i.get('token'), "id":i.get('id')}
+        customer_lists.append(cust)
 
-r = requests.get(url,params={"kategori":"telefon"})
-print r.ok
+    ## benzer sekilde post_list olusturup verileri json'a cevirelim.
+    post_list_url = "http://yourapi.com/api/get-post-list"
+    post_list = requests.get(post_list_url)
+    json_posts = json.loads(post_list.text)
 
-r = requests.post(url,data={"username":"arda","pass":"1234"})
-print r.status_code
+    post_list_id = []
+    for i in json_posts:
+        post_list_id.append(i.get('id'))
 
-r = requests.post(url,headers={"User-Agent":"Soyluarda modified"})
+    # bu kısımda url' tanımladıktan sonra göndermek istediğimizi parametreleri de params ile yolluyoruz.
+    add_comment_url = "http://yourapi.com/api/add-comment"
+    add_comment = requests.post(add_comment_url,params={"token":customer_lists[id].get('token'),"postId":post_list_id[post_id],"comment":"comment added","name":"Arda Soylu"})
 
-r = requests.post(url,headers={"Referer":"previoussite.com"})
+
+for i in range(5):
+    Thread(target=add_comment(i,3),args=(i),).start()
